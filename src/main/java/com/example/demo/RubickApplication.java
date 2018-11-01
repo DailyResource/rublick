a246e5586c87ca025aacdd911fedd1eca730f308
ertai2018 <359915341@qq.com>
2018-11-01 14:44:54 +0800

    基础框架代码

---
+++ src/main/java/com/example/demo/RubickApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RubickApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubickApplication.class, args);
+	}
