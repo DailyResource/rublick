6d57910341020f3d122a9c068c067ccdc695d2a4
ertai2018 <359915341@qq.com>
2018-11-01 16:56:59 +0800

    Update src/main/java/online/rubick/applications/controller/VerificationController.java

--- src/main/java/online/rubick/applications/controller/VerificationController.java
+++ src/main/java/online/rubick/applications/controller/VerificationController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,10 +26,6 @@ public class VerificationController {
 	
 	@Autowired  
 	DefaultKaptcha defaultKaptcha; 
-	
-	@Value("${server.contextPath}")
-	private String contextPath;
-	
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/verifycode")
 	public void verifycode(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse)throws Exception {
