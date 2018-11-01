fe4bd79fc38c24b2f814b383690c783bc33a8b9c
ertai2018 <359915341@qq.com>
2018-11-01 14:57:11 +0800

    修改bug

--- src/main/java/com/example/demo/RubickApplication.java
+++ src/main/java/com/example/demo/RubickApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,3 +10,4 @@ public class RubickApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(RubickApplication.class, args);
 	}
+}
