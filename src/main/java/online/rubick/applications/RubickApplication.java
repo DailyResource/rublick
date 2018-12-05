9cf1d84fd88ab3603f74f77d3d5c0027843fc098
ertai2018 <359915341@qq.com>
2018-12-05 08:12:30 +0800

    SSL证书

--- src/main/java/online/rubick/applications/RubickApplication.java
+++ src/main/java/online/rubick/applications/RubickApplication.java
@@ -1,9 +1,13 @@
 package online.rubick.applications;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -18,4 +22,17 @@ public class RubickApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RubickApplication.class, args);
 	}
+	
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
+    
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8001);
+        return connector;
+    }
 }
