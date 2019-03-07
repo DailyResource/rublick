2f59741864c79670359602bf287662c11a0d691b
ertai2018 <359915341@qq.com>
2019-03-07 16:44:43 +0800

    补充

--- src/main/java/online/rubick/applications/controller/wx/WXGroupController.java
+++ src/main/java/online/rubick/applications/controller/wx/WXGroupController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,45 +14,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import online.rubick.applications.entity.rubick.Files;
+import online.rubick.applications.controller.web.GroupController;
 import online.rubick.applications.entity.rubick.FilesGroup;
-import online.rubick.applications.enums.rubick.FileStatusEnum;
 import online.rubick.applications.enums.rubick.GroupTypeEnum;
+import online.rubick.applications.exception.ApplicationException;
 import online.rubick.applications.security.annotation.SessionAccount;
 import online.rubick.applications.service.rubick.FilesGroupService;
-import online.rubick.applications.service.rubick.FilesService;
 import online.rubick.applications.vo.rubick.FilesGroupVO;
 import online.rubick.applications.vo.rubick.FilesVO;
 import online.rubick.applications.vo.sys.UserVO;
 
-@Api(description = "分组管理")
+@Api(description = "分组管理-微信")
 @RestController
 @RequestMapping("/wx/group")
 @ResponseBody
 public class WXGroupController {
+
+	@Autowired
+	private GroupController webGroupController;
 	@Autowired
 	private FilesGroupService filesGroupService;
-	@Autowired
-	private FilesService filesService;
+
+	@ApiOperation(value = "test")
+	@GetMapping(value = "/test")
+	public FilesVO test() {
+		FilesVO vo = new FilesVO();
+		vo.setRemark("测试Remark");
+		System.out.println("测试");
+		return vo;
+	}
 
 	@ApiOperation(value = "根据分组获取图片集合")
 	@GetMapping(value = "/getPhotoList")
 	public List<FilesVO> getPhoto(@RequestParam("groupId") String groupId) {
-		List<Files> filesList = filesService.findByGroupId(groupId);
-		List<FilesVO> list = new ArrayList<>();
-		for (Files files : filesList) {
-			FilesVO vo = new FilesVO();
-			BeanUtils.copyProperties(files, vo);
-			vo.setStatusName(FileStatusEnum.getEnumByCode(files.getStatus()).getDescription());
-			list.add(vo);
-		}
-		return list;
+		return webGroupController.getPhoto(groupId);
 	}
 
-	@ApiOperation(value = "获取分组")
+	@ApiOperation(value = "获取商品分组")
 	@GetMapping("getGroup")
-	public List<FilesGroupVO> getGroup(@SessionAccount UserVO userVO) {
-		List<FilesGroup> filesGroups = filesGroupService.findByUserIdAndType(userVO.getUserId(), null);
+	public List<FilesGroupVO> getGroup(@RequestParam("userId") String userId) {
+		List<FilesGroup> filesGroups = filesGroupService.findByUserIdAndType(userId, GroupTypeEnum.DIY.getCode());
 		List<FilesGroupVO> list = new ArrayList<>();
 		for (FilesGroup filesGroup : filesGroups) {
 			FilesGroupVO vo = new FilesGroupVO();
@@ -61,11 +63,10 @@ public class WXGroupController {
 		return list;
 	}
 
-	@ApiOperation(value = "获取下落图标")
+	@ApiOperation(value = "获取下落分组")
 	@GetMapping("getDropIcon")
-	public FilesGroupVO getDropIcon(@SessionAccount UserVO userVO) {
-		List<FilesGroup> filesGroups = filesGroupService.findByUserIdAndType(userVO.getUserId(),
-				GroupTypeEnum.DROP.getCode());
+	public FilesGroupVO getDropIcon(@RequestParam("userId") String userId) {
+		List<FilesGroup> filesGroups = filesGroupService.findByUserIdAndType(userId, GroupTypeEnum.DROP.getCode());
 		if (filesGroups.isEmpty()) {
 			return null;
 		}
